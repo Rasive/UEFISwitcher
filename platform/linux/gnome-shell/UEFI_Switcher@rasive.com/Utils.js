1c85f4720e2eb505411ae8d9d6662457f88c78a2
Rasmus Zweidorff Iversen <rasmus.z.iversen@gmail.com>
2018-05-15 20:37:01 +0200

    Renamed Utils to utils

--- platform/linux/gnome-shell/UEFI_Switcher@rasive.com/Utils.js
+++
@@ -1,9 +0,0 @@
-const Extension = imports.misc.extensionUtils.getCurrentExtension();
-
-function log(text) {
-    global.log("[" + Extension.metadata.uuid + "]", text);
-}
-
-function log(tag, text) {
-    global.log("[" + Extension.metadata.uuid + "::" + tag + "]", text);
-}
