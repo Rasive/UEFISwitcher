6e475bc56b3c0ab5972de3a2bccd49cc5e1bb7e4
Rasmus Zweidorff Iversen <rasmus.z.iversen@gmail.com>
2018-05-18 11:49:25 +0200

    Added log levels

--- platform/linux/gnome-shell/UEFI_Switcher@rasive.com/logger.js
+++ platform/linux/gnome-shell/UEFI_Switcher@rasive.com/logger.js
@@ -2,11 +2,26 @@ const Extension = imports.misc.extensionUtils.getCurrentExtension();
 
 const self = this;
 
+const NONE = -1;
+const WARNING = 10;
+const ERROR = 20;
+const FATAL = 30;
+const DEBUG = 40;
+
+let level = self.DEBUG;
+
+function setLevel(level) {
+    self.level = level;
+}
+
 function debug(tag) {
+    if (self.level < self.DEBUG)
+        return;
+
     let args = [];
     Array.prototype.push.apply(args, arguments);
 
-    if(typeof tag != "undefined") {
+    if (typeof tag != "undefined") {
         args.shift();
     }
 
@@ -25,7 +40,7 @@ function _buildArgsStr(args) {
 
     let str = "";
 
-    for(let i = 0; i < args.length; i++) {
+    for (let i = 0; i < args.length; i++) {
         const arg = args[i];
         if (Array.isArray(arg)) {
             str += self._arrayToString(arg);
@@ -52,6 +67,8 @@ function _arrayToString(array) {
             str += self._arrayToString(element);
         } else if (typeof element == "object") {
             str += self._objToString(element);
+        } else if (typeof element == "string") {
+            str += "\"" + element + "\"";
         } else {
             str += element;
         }
@@ -81,10 +98,13 @@ function _objToString(obj) {
             continue;
 
         let val = obj[key];
+
         if (typeof obj[key] == "object") {
             val = self._objToString(obj[key]);
-        } else if (typeof (obj[key] == "array")) {
+        } else if (typeof obj[key] == "array") {
             val = self._arrayToString(obj[key]);
+        } else if (typeof obj[key] == "string") {
+            val = "\"" + obj[key] + "\"";
         }
 
         str += "\"" + key + "\" : " + val;
